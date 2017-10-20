2a6c476c5c7c4144c7260d63f03da75fa693a454
Robert Tuck <robtuck@gmail.com>
2017-10-20 14:55:54 -0400

    added get /minions/:miniondsId.  moved minion lookup to minionsRouter.param middleware

--- server/minions.js
+++ server/minions.js
@@ -12,7 +12,20 @@ const minionsRouter = express.Router();
 
 module.exports = minionsRouter;
 
+minionsRouter.param('minionId', (req, res, next, id) => {
+  const foundMinion = getFromDatabaseById('minions', req.params.minionId);
+  if (foundMinion) {
+    req.minion = foundMinion;
+    next();
+  } else {
+    res.status(404).send();
+  }
+})
 // Get all expressions
 minionsRouter.get('/', (req, res, next) => {
   res.send(getAllFromDatabase('minions'));
+});
+
+minionsRouter.get('/:minionId', (req, res, next) => {
+  res.send(req.minion);
 });
