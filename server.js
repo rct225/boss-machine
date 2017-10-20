3fce95c5825fc043733dfcd515329056fbf69e26
Robert Tuck <robtuck@gmail.com>
2017-10-20 13:52:55 -0400

    added body-parser and cors middleware.  mounted route at /api.  started listener on given port

--- server.js
+++ server.js
@@ -9,17 +9,28 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+var cors = require('cors')
+app.use(cors());
 
 // Add middware for parsing request bodies here:
+const bodyParser = require('body-parser')
 
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
 
+app.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
-if (!module.parent) { 
+if (!module.parent) {
   // Add your code to start the server listening at PORT below:
+  const server = http.createServer(requestHandler);
 
+  server.listen(port, (err) => {
+    if (err) {
+      return console.log('Server did not start succesfully: ', err);
+    }
+
+    console.log(`Server is listening on ${port}`);
+  });
 }
