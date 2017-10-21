a1e5a3889b6ad85dbed6219fb0055076e73172fe
Robert Tuck <robtuck@gmail.com>
2017-10-21 19:48:41 -0400

    added meetings.js to implement meeting get, post and delete routes

--- server/api.js
+++ server/api.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const apiRouter = express.Router();
 
-
-
-module.exports = apiRouter;
-
 const minionsRouter = require('./minions');
+const ideasRouter = require('./ideas');
+const meetingsRouter = require('./meetings');
 
 apiRouter.use('/minions', minionsRouter);
-
-const ideasRouter = require('./ideas');
-
 apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
+
+module.exports = apiRouter;
